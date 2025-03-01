79bfd0046c338af8973da82a541b7709a1dc16a9
Mireia Majó i Cornet <78376277+MireiaMajoCornet@users.noreply.github.com>
2025-03-01 17:50:31 +0100

    changed prediction format

--- week2/yolo/utils/format_yolo_predictions.py
+++ week2/yolo/utils/format_yolo_predictions.py
@@ -2,30 +2,30 @@ import numpy as np
 
 # TODO handle frames with no predictions
 
-def format_yolo_predictions(preds, class_mapping = {0: 'car', 1: 'bike'}):
+def format_yolo_predictions(preds, frames_list, class_mapping = {0: 'car', 1: 'bike'}):
     """
     Formats YOLO predictions into a more readable structure.
     Args:
         preds (list): A list of prediction objects, where each prediction contains bounding boxes, confidence scores, and class indices.
         class_mapping (dict, optional): A dictionary mapping class indices to class names. Defaults to {0: 'car', 1: 'bike'}.
     Returns:
-        list: A list of formatted predictions. Each formatted prediction is a list of dictionaries, where each dictionary contains:
+        list: A list of formatted predictions. Each formatted prediction is a dictionary of dictionaries, where each dictionary contains:
             - 'bbox' (numpy.ndarray): The bounding box coordinates.
-            - 'conf' (float): The confidence score.
             - 'class' (str): The class name.
+            - 'conf' (float): The confidence score.
     """
 
     formatted_preds = []
 
-    for pred in preds:
-        formatted_dets = []
+    for pred, frame in zip(preds, frames_list):
+        formatted_dets = {}
 
         for box in pred.boxes.cpu():
 
             for coords, conf, cls in zip(box.xyxy, box.conf, box.cls):
-                det = {'bbox': coords.numpy(), 'conf': conf.item(), 'class': class_mapping[int(cls.item())]}
+                det = {'bbox': coords.numpy(), 'class': class_mapping[int(cls.item())], 'conf': conf.item()}
 
-            formatted_dets.append(det)
+            formatted_dets[frame] = det
         formatted_preds.append(formatted_dets)
     
     return formatted_preds
