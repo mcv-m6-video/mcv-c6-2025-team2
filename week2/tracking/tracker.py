589d584bdceab4d02e919b2646ff054470f43819
abriilp <101658222+abriilp@users.noreply.github.com>
2025-03-05 14:53:46 +0100

    overlap tracker visu i results

--- week2/tracking/tracker.py
+++ week2/tracking/tracker.py
@@ -49,6 +49,7 @@ class Tracker:
                     continue
                 
                 iou_score = self.iou(prev_box, det['bbox'])
+                #print(iou_score)
                 if iou_score > best_iou:
                     best_iou = iou_score
                     best_match = i
@@ -78,43 +79,6 @@ class Tracker:
                         cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
         return frame
     
-    def read_gt_ids(self, file_path: str) -> dict:
-        "Reads the GT file and returns a dictionary prepared for the HOTA metric."
-        
-        # Parse the XML file
-        tree = ET.parse(file_path)
-        root = tree.getroot()
-
-        # Inicializamos los datos que necesitamos
-        gt_ids = defaultdict(list)
-        num_gt_dets = 0
-        unique_ids = set()
-
-        # Iteramos a través de los tracks
-        for track in root.findall('track'):
-            track_id = int(track.get('id'))
-            unique_ids.add(track_id)  # Agregar track_id al conjunto de IDs únicos
-            frames = []
-
-            # Iteramos a través de las cajas dentro de cada track
-            for box in track.findall('box'):
-                frame = int(box.get('frame'))
-                # Almacenamos el ID del track en cada frame
-                gt_ids[frame].append(track_id)
-                num_gt_dets += 1
-
-            # No es necesario contar el número de IDs únicos aquí porque lo hacemos con el conjunto unique_ids
-
-        num_gt_ids = len(unique_ids)  # Número total de IDs únicos
-        
-
-        # Creamos el diccionario con los resultados
-        self.tracking_data["num_gt_dets"] = num_gt_dets
-        self.tracking_data["num_gt_ids"] = num_gt_ids,
-        self.tracking_data["gt_ids"] = [np.array(gt_ids[frame]) for frame in sorted(gt_ids.keys()) if frame >= 535]
-
-        return 
-    
     def compute_similarity_matrix(self, gt_bboxes, detected_bboxes):
         """
         Computes IoU similarity between all ground truth and detected bounding boxes in a specific frame.
@@ -133,9 +97,9 @@ class Tracker:
                 similarity_matrix[i, j] = self.iou(gt_bbox, det_bbox)
 
         return similarity_matrix
-    
 
-    def parse_bboxes_from_xml(self, file_path):
+    
+    def load_gt_annotations_xml(self, file_path, split_index):
         # Parsear el archivo XML
         tree = ET.parse(file_path)
         root = tree.getroot()
@@ -143,15 +107,22 @@ class Tracker:
         # Crear un diccionario para almacenar las coordenadas por frame
         frames_dict = {}
 
+        gt_ids = defaultdict(list)
+        num_gt_dets = 0
+        unique_ids = set()
+
         # Iterar sobre los elementos <track>
         for track in root.findall('track'):
-            # Iterar sobre los elementos <box> dentro de cada <track>
+            track_id = int(track.get('id'))
+            unique_ids.add(track_id) 
+            frames = []
+
             for box in track.findall('box'):
                 # Obtener el número de frame
                 frame_number = int(box.get('frame'))
 
                 # Solo procesar frames desde el 535 en adelante
-                if frame_number >= 535:
+                if frame_number >= split_index:
                     # Obtener las coordenadas del bounding box
                     xtl = float(box.get('xtl'))
                     ytl = float(box.get('ytl'))
@@ -165,16 +136,87 @@ class Tracker:
                     # Añadir las coordenadas del bounding box al frame correspondiente
                     frames_dict[frame_number].append([xtl, ytl, xbr, ybr])
 
+                    gt_ids[frame_number].append(track_id)
+                    num_gt_dets += 1
+
+            num_gt_ids = len(unique_ids)  # Número total de IDs únicos
+            
+        # Creamos el diccionario con los resultados
+        self.tracking_data["num_gt_dets"] = num_gt_dets
+        self.tracking_data["num_gt_ids"] = num_gt_ids,
+        self.tracking_data["gt_ids"] = [np.array(gt_ids[frame]) for frame in sorted(gt_ids.keys()) if frame >= split_index]
+
         # Crear una lista de listas ordenada por los frames desde el 535 en adelante
         if frames_dict:
-            min_frame = 535
             max_frame = max(frames_dict.keys())  # Obtener el último frame presente
             frame_list = []
 
-            for i in range(min_frame, max_frame + 1):
+            for i in range(split_index, max_frame + 1):
                 frame_list.append(frames_dict.get(i, []))  
             self.gt_detections = frame_list
             #return frame_list
         else:
             self.gt_detections = []
             #return []  
+
+    def draw_tracks2(self, frame, object_colors):
+        result_image = frame.copy()
+
+        for track_id, bbox in self.tracks.items():
+            x1, y1, x2, y2 = map(int, bbox)
+            # Assign a unique color if the object ID is new
+            if track_id not in object_colors:
+                np.random.seed(track_id)  # Seed with object ID to ensure consistency
+                object_colors[track_id] = tuple(map(int, np.random.randint(0, 255, size=3)))
+            
+            color = object_colors[track_id]
+            
+            # Draw the detection rectangle
+            cv2.rectangle(result_image, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
+            
+            # Add text with the object ID
+            cv2.putText(
+                result_image, 
+                f"ID:{track_id}", 
+                (int(x1), int(y1) - 5), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                0.5, 
+                color, 
+                2
+            )
+        
+        return result_image, object_colors
+    
+    def visualize_tracked_objects(self, image, tracked_objects, object_colors):
+        """
+        Draw tracked objects on the image with their IDs, using distinct colors for each object.
+        """
+        # Create a copy of the image to avoid modifying the original
+        result_image = image.copy()
+        
+        for d in tracked_objects:
+            x1, y1, x2, y2, obj_id = d  # Unpack coordinates and object ID
+            obj_id = int(obj_id)
+            
+            # Assign a unique color if the object ID is new
+            if obj_id not in object_colors:
+                np.random.seed(obj_id)  # Seed with object ID to ensure consistency
+                object_colors[obj_id] = tuple(map(int, np.random.randint(0, 255, size=3)))
+            
+            color = object_colors[obj_id]
+            
+            # Draw the detection rectangle
+            cv2.rectangle(result_image, (int(x1), int(y1)), (int(x2), int(y2)), color, 2)
+            
+            # Add text with the object ID
+            cv2.putText(
+                result_image, 
+                f"ID:{obj_id}", 
+                (int(x1), int(y1) - 5), 
+                cv2.FONT_HERSHEY_SIMPLEX, 
+                0.5, 
+                color, 
+                2
+            )
+        
+        return result_image, object_colors
